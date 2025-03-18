5690dc895bfff6e5ae37e1b279d6fb0f43e775bb
belibeerda <belitskkirill@gmail.com>
2025-03-18 17:21:55 +0300

    Сладер категорий, адаптация каталога

--- js/script.js
+++ js/script.js
@@ -18,3 +18,37 @@ if (menu && menuBtn) {
         })
     })
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const slider = document.querySelector(".categories-grid-container");
+    const items = document.querySelectorAll(".categories-grid-container-item");
+    const itemWidth = items[0].offsetWidth + 60;
+    const maxIndex = items.length - 6;
+    let currentIndex = 0;
+
+    const prevButton = document.querySelector(".prev");
+    const nextButton = document.querySelector(".next");
+
+    function updateButtons() {
+        prevButton.disabled = currentIndex === 0;
+        nextButton.disabled = currentIndex >= maxIndex;
+    }
+
+    prevButton.addEventListener("click", () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+            slider.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+            updateButtons();
+        }
+    });
+
+    nextButton.addEventListener("click", () => {
+        if (currentIndex < maxIndex) {
+            currentIndex++;
+            slider.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+            updateButtons();
+        }
+    });
+
+    updateButtons();
+});
